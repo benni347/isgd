a0553604b57021a4b9883761cddda190a2a14458
Cedric <cdrc+github@skwar.me>
2020-05-12 21:58:43 +0200

    input ausgelesen wird zu output

--- app/src/main/java/com/example/isgd/MainActivity.java
+++ app/src/main/java/com/example/isgd/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.isgd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void test(View view) {
+        TextView input = (TextView) findViewById(R.id.inpout);
+        String inputurl = (String) input.getText();
+        try {
+            inputurl = URLEncoder.encode(inputurl, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+
+        TextView output = (TextView) findViewById(R.id.outpout);
+        output.setText(inputurl);
+
+
     }
 }
