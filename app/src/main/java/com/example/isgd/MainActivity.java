b9231195e09638b796fbfe7cde8f9d4c5b2f5de0
Alexander Skwar <alexander@tutti.ch>
2020-07-10 17:23:29 +0200

    Optimize imports.

--- app/src/main/java/com/example/isgd/MainActivity.java
+++ app/src/main/java/com/example/isgd/MainActivity.java
@@ -15,7 +15,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
