1d198638e5da03335eb0f92cdf337bc5bb476ac6
Alexander Skwar <alexander@tutti.ch>
2020-07-11 18:37:15 +0200

    Rewritten to use Intent service for network connection.
    No more network on main thread.

--- app/src/main/java/com/example/isgd/MainActivity.java
+++ app/src/main/java/com/example/isgd/MainActivity.java
@@ -1,120 +1,103 @@
 package com.example.isgd;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int DOWNLOAD_REQUEST_CODE = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        StrictMode.ThreadPolicy policy = new
-                StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
     }
 
-    public static String executePost(String targetURL, String urlParameters) {
-        HttpURLConnection connection = null;
-
-        try {
-            // Create connection
-            URL url = new URL(targetURL);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type",
-                    "application/x-www-form-urlencoded");
-
-            connection.setRequestProperty("Content-Length",
-                    Integer.toString(urlParameters.getBytes().length));
-            connection.setRequestProperty("Content-Language", "en-US");
-
-            connection.setUseCaches(false);
-            connection.setDoOutput(true);
-
-            // Send request
-            DataOutputStream wr = new DataOutputStream (
-                    connection.getOutputStream());
-            wr.writeBytes(urlParameters);
-            wr.close();
-
-            // Get Response
-            InputStream is = connection.getInputStream();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-            StringBuilder response = new StringBuilder();
-            String line;
-            while ((line = rd.readLine()) != null) {
-                response.append(line);
-                response.append('\r');
-            }
-            rd.close();
-            return response.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
-    }
-
-    private String readStream(InputStream is) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        BufferedReader r = new BufferedReader(new InputStreamReader(is),1000);
-        for (String line = r.readLine(); line != null; line =r.readLine()){
-            sb.append(line);
-        }
-        is.close();
-        return sb.toString();
-    }
-
-    public void test(View view) {
+    public void click_kuerzen(View view) {
+        // Find the "input" field
         TextView input = findViewById(R.id.inpout);
+        // Read the "long" original URL
+        String inputUrl = input.getText().toString();
 
-        // Does the user want statistics to be logged?
-        boolean logStats = ((CheckBox) findViewById(R.id.stats)).isChecked();
-
-        // Read the "long" input URL
-        String inputurl = input.getText().toString();
         try {
             // Encode the URL (convert chars like ":" and "&")
-            inputurl = URLEncoder.encode(inputurl, StandardCharsets.UTF_8.toString());
+            inputUrl = URLEncoder.encode(inputUrl, StandardCharsets.UTF_8.toString());
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
 
         // Build the string with the url parameters (after the "create.php")
-        String urlParameters = "format=simple" + "&url=" + inputurl;
+        String urlParameters = "format=simple" + "&url=" + inputUrl;
 
         // Are stats to be logged?
-        // If so, append "logstats=1" to urlParameters
+        // If so, append "logstats=1" to url
+        // Find the checkbox "log stats?"
+        CheckBox cb_stats = findViewById(R.id.stats);
+        // Does the user want statistics to be logged?
+        boolean logStats = cb_stats.isChecked();
         if (logStats) urlParameters = urlParameters + "&logstats=1";
 
-        // Is there an inputurl which is to be shortened?
-        // Only do a http call, IF there's a long URL
-        if (inputurl != null && !inputurl.isEmpty()) {
-            String shortURL;
-            shortURL = MainActivity.executePost(getString(R.string.create_url_base), urlParameters);
+        PendingIntent pendingResult = createPendingResult(
+                DOWNLOAD_REQUEST_CODE, new Intent(), 0);
 
-            TextView output = findViewById(R.id.outpout);
-            output.setText(shortURL);
+        // Is there an input url which is to be shortened?
+        // Only do a http call, IF there's a long URL
+        if (inputUrl != null && !inputUrl.isEmpty()) {
+            Intent intent = new Intent(getApplicationContext(), ShortenUrlIntentService.class);
+            intent.putExtra(ShortenUrlIntentService.URL_PARAMETERS_EXTRA, urlParameters);
+            intent.putExtra(ShortenUrlIntentService.TARGET_URL_EXTRA, getString(R.string.create_url_base));
+            intent.putExtra(ShortenUrlIntentService.PENDING_RESULT_EXTRA, pendingResult);
+            startService(intent);
         }
+    }
+
+    /*
+     This method gets called when the ShortenUrlIntentService is finished.
+     It will either print the shortened URL or show an error message.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == DOWNLOAD_REQUEST_CODE) {
+            switch (resultCode) {
+                case ShortenUrlIntentService.INVALID_URL_CODE:
+                    handleInvalidURL();
+                    break;
+                case ShortenUrlIntentService.ERROR_CODE:
+                    handleError(data);
+                    break;
+                case ShortenUrlIntentService.RESULT_CODE:
+                    handleShortURL(data);
+                    break;
+            }
+            handleShortURL(data);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleShortURL(Intent data) {
+        TextView output;
+        String shortURL;
+
+        shortURL = data.getStringExtra(ShortenUrlIntentService.URL_RESULT_EXTRA);
+        output = findViewById(R.id.outpout);
+        output.setText(shortURL);
+    }
+
+    private void handleError(Intent data) {
+        // whatever you want
+    }
+
+    private void handleInvalidURL() {
+        // whatever you want
     }
 }
